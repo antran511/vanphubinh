12fe92786d0805742fdf721aa14c679979cdf73b
an-quodai <an.tran511@hotmail.com>
2023-07-22 22:26:53 +0800

    Install theme for semi design

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import semi from "vite-plugin-semi-theme";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    semi({
+      theme: "@semi-bot/semi-theme-universedesign",
+    }),
+  ],
 });
